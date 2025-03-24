4ff1a675e947999bd6fde41dd06c5355ab9a7d89
徐凯 <xukai@yxgsyf.com>
2025-03-24 10:55:30 +0800

    更新index.html和相关页面，修正导航链接，确保指向正确的路径；优化CSS样式，添加新样式类以提升页面布局和可读性；更新package.json以引入新依赖，增强构建和部署流程。

--- dist/tailwind.config.js
+++ dist/tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./**/*.{html,js}",
+    "./*.html",
+    "./models/**/*.html",
+    "./common/js/**/*.js",
     "./common/styles/**/*.css",
   ],
   darkMode: "class",
